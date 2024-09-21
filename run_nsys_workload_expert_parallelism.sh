fbe4d34ebccb7ff9ec7b8033b61394e475c7b160
Shamauk <zacharydoucet01@hotmail.com>
2024-09-21 09:29:31 -0400

    Cleanup

--- run_nsys_workload_expert_parallelism.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-read -p "Choose filename (naive, bmm, demeter, adnexus, etc): " file_name
-
-nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas -s cpu  --force-overwrite=true --capture-range=cudaProfilerApi --cudabacktrace=true -x true -o my_profile python3 run_workload_expert_parallelism.py $file_name
