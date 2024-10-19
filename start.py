70f47f8d1327174c5c16a07faefe374d72d71a01
Shamauk <zacharydoucet01@hotmail.com>
2024-10-19 09:55:50 -0400

    cleanup

--- start.py
+++ start.py
@@ -43,8 +43,6 @@ parser.add_argument("-d", "--dataset", default="sst2", type=str)
 parser.add_argument("-bs", "--batch_size", default=250, type=int, help="Batch size per GPU")
 parser.add_argument("-x", "--experiment", default="standard", type=str)
 parser.add_argument("-ec", "--expert_cache_size", default=2, type=int)
-# parser.add_argument("-e", "--num_experts", default=8, type=int)
-# parser.add_argument("-mt", "--model_type", default="encoder-decoder", type=str)
 parser.add_argument("-pa", "--path", default="outputs", type=str, help="Specify where to save path")
 parser.add_argument("-eq", "--eq_tokens", default=150, type=int)
 parser.add_argument("-dm", "--d_model", default=768, type=int)
@@ -58,19 +56,10 @@ parser.add_argument("-sys", "--system", default="harmony", type=str)
 
 args = parser.parse_args()
 
-# if args.num_experts not in [8, 16, 32, 64, 128, 256]:
-#     print(f"There is no model with {args.num_experts} experts")
-#     exit(1)
-
 ## Validation
 if args.num_iters == 0 and args.num_samples == 0:
     print("You must either specify --num_iters or --num_samples")
     exit(1)
-
-# if args.num_iters != 0:
-#     DESIRED_DATASET_SIZE = 
-# else:
-#     DESIRED_DATASET_SIZE = args.num_samples
 
 def setup(rank):
     os.environ["MASTER_ADDR"] = "localhost"
@@ -83,18 +72,6 @@ def setup(rank):
 def cleanup():
     dist.destroy_process_group()
 
-# def move_to_cuda_except_experts(model):
-#     for name, module in model.named_children():
-#         if name == 'experts':
-#             # We want to keep the experts on cpu
-#             continue
-#         elif list(module.children()):
-#             # If the module has children, recurse
-#             move_to_cuda_except_experts(module)
-#         else:
-#             # If it's a leaf module (no children) and not part of experts, move to CUDA
-#             module.cuda()
-
 def run_inference_workload(rank):
     try:
         mp.current_process().name = f'Worker-{rank}'
@@ -103,20 +80,9 @@ def run_inference_workload(rank):
 
         tokenizer = AutoTokenizer.from_pretrained(args.model_name, cache_dir="/cache")
         
-        # if args.model_type == "encoder-decoder":
-        #     _class = SwitchTransformersForConditionalGeneration
-        # elif args.model_type == "encoder":
-        #     _class = SwitchTransformersEncoderModel
-        # else:
-        #     print("That model type is not yet implemented!")
-        #     exit(1)
-        
         model = Modelling(**vars(args))
         model.cuda()
         model.eval()
-
-        # move_to_cuda_except_experts(model)
-        # model.expert_parallelise()
 
         flexible_dataset = FlexibleDataset(
             args.dataset, 
@@ -159,19 +125,12 @@ def run_standard_experiment(model, tokenizer, loader, path):
             start = time.time()
             batch = {k: v.cuda() for k, v in batch.items()}
             model(batch)
-            # outputs = model(**batch)
-            # if args.model_type == "encoder-decoder":
-            #     probs = F.softmax(outputs.logits, dim=-1)
-            #     predicted_token_ids = torch.argmax(probs, dim=-1)
-            #     predicted_token = [tokenizer.decode(pred_id, skip_special_tokens=True) for pred_id in predicted_token_ids]
             end = time.time()
             latencies.append(end-start)
     
 
     create_save_dir_if_not_exist(path)
     model.save_statistics(path)
-    # for moe_layer in moe_layers:
-    #     moe_layer.save_statistics(DIR=path)
 
     file_path = f"{path}/e2e.csv"
     with open(file_path, "w") as f:
@@ -215,27 +174,6 @@ def create_save_dir_if_not_exist(path):
 def save_run_info(path):
      with open(f"{path}/data.json", "w") as f:
         json.dump(vars(args), f)
-        # json.dump({
-        #     "scheduling_policy": args.schedule,
-        #     "cache_policy": args.cache_policy,
-        #     "batch_size": args.batch_size,
-        #     "world_size": args.world,
-        #     "dataset": args.dataset,
-        #     "model": args.model,
-        #     "seq_len": args.seq_len,
-        #     "num_iters": args.num_iters,
-        #     "num_samples": args.num_samples,
-        #     "port": args.port,
-        #     "experiment": args.experiment,
-        #     "expert_cache_size": args.expert_cache_size,
-        #     "eq_tokens": args.eq_tokens,
-        #     "d_model": args.d_model,
-        #     "name_moe_layer": args.name_moe_layer,
-        #     "name_router": args.name_router,
-        #     "name_experts": args.name_experts,
-        #     "name_decoder": args.name_decoder,
-        #     "dynamic_components": args.dynamic_components,
-        # }, f)
 
 def signal_handler(sig, frame):
     print("Main process received Ctrl+C! Terminating all child processes...")
