c3a3383e499fb6329fbb18c861e8bc5552f4b346
Shamauk <zacharydoucet01@hotmail.com>
2024-11-29 19:58:25 -0500

    Improved the create_csv_timebreakdown script to work with any number of latencies automatically

--- plotting/create_csv_timebreakdown.py
+++ plotting/create_csv_timebreakdown.py
@@ -10,19 +10,16 @@ num_gpus = 8
 path = sys.argv[1]
 df = pd.DataFrame()
 for i in range(num_layers):
-    _df = []
+    _df = pd.DataFrame()
     for j in range(num_gpus):
         __df = pd.read_csv(f"{path}/{j}/moe_layer-{i}.csv")
+        __df = __df.loc[:, __df.columns.str.contains(r"\(ms\)", regex=True)]
         __df = __df.sum(axis=0)
-        _df.append({
-            "rank": j,
-            "latency (ms)": __df["latency (ms)"],
-            "metadata latency (ms)": __df["metadata latency (ms)"],
-            "first transfer latency (ms)": __df["first transfer latency (ms)"],
-            "second transfer latency (ms)": __df["second transfer latency (ms)"],
-            "comp latency (ms)": __df["comp latency (ms)"],
-        })
-    _df = pd.DataFrame(_df)
+        __df["rank"] = j
+
+        _df = pd.concat([_df, __df.to_frame().T], ignore_index=True)
+
+    _df["rank"] = _df["rank"].astype(int)
     max_comp = _df["comp latency (ms)"].max()
     _df["wait latency (ms)"] = max_comp - _df["comp latency (ms)"]
     _df["second transfer latency (ms)"] -= _df["wait latency (ms)"]
