7c92a0d3fb0f7f3121ff97911c52ccef1adc74ec
Shamauk <zacharydoucet01@hotmail.com>
2026-02-17 01:16:54 -0500

    Get rid of info that stores testing scripts and data fetching scripts for model info

--- info/get_mixtral_arch.py
+++
@@ -1,7 +0,0 @@
-import sys
-from transformers import AutoModel
-
-with open("mixtral_arch.txt", "w") as f:
-    model = AutoModel.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1", cache_dir="/cache")
-    sys.stdout = f
-    print(model)
