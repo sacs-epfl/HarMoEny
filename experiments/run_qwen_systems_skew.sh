ffe5e6bef86e05d46f1cb38cd1e23fa34be187a1
Shamauk <zacharydoucet01@hotmail.com>
2026-02-17 01:07:52 -0500

    Cleaned experiments

--- experiments/run_qwen_systems_skew.sh
+++ experiments/run_qwen_systems_skew.sh
@@ -19,26 +19,26 @@ for skew_index in "${!skews[@]}"
 do
     skew="${skews[$skew_index]}"
     batch_size=${fastmoe_batches[$skew_index]}
-    
-    # python3 src/start_fastmoe.py \
-    #     --system_name fastmoe \
-    #     --dataset "constant" \
-    #     --num_samples $num_samples \
-    #     --batch_size $batch_size \
-    #     --seq_len $seq_len \
-    #     --model_name "Qwen/Qwen1.5-MoE-A2.7B-Chat" \
-    #     --num_experts 60 \
-    #     --d_model 2048 \
-    #     --type_moe_parent Qwen2MoeDecoderLayer \
-    #     --type_moe Qwen2MoeSparseMoeBlock \
-    #     --router_tensor_path gate \
-    #     --name_experts experts \
-    #     --world_size $world_size \
-    #     --enable_router_skew $enable_skew \
-    #     --router_skew $skew \
-    #     --router_num_experts_skewed $num_experts_skewed \
-    #     --pa "outputs/exp-qwen-systems-skew/$datetime/$skew-fastmoe"
-    
+
+    python3 src/start_fastmoe.py \
+        --system_name fastmoe \
+        --dataset "constant" \
+        --num_samples $num_samples \
+        --batch_size $batch_size \
+        --seq_len $seq_len \
+        --model_name "Qwen/Qwen1.5-MoE-A2.7B-Chat" \
+        --num_experts 60 \
+        --d_model 2048 \
+        --type_moe_parent Qwen2MoeDecoderLayer \
+        --type_moe Qwen2MoeSparseMoeBlock \
+        --router_tensor_path gate \
+        --name_experts experts \
+        --world_size $world_size \
+        --enable_router_skew $enable_skew \
+        --router_skew $skew \
+        --router_num_experts_skewed $num_experts_skewed \
+        --pa "outputs/exp-qwen-systems-skew/$datetime/$skew-fastmoe"
+
     python3 src/start_fastmoe.py \
         --system_name fastermoe \
         --dataset "constant" \
@@ -57,22 +57,22 @@ do
         --router_skew $skew \
         --router_num_experts_skewed $num_experts_skewed \
         --pa "outputs/exp-qwen-systems-skew/$datetime/$skew-fastermoe"
-    
-    # deepspeed --num_gpus $world_size src/start_deepspeed.py \
-    #     --dataset "constant" \
-    #     --num_samples $num_samples \
-    #     --batch_size ${deepspeed_batches[$skew_index]} \
-    #     --seq_len $seq_len \
-    #     --model_name "Qwen/Qwen1.5-MoE-A2.7B-Chat" \
-    #     --num_experts 60 \
-    #     --d_model 2048 \
-    #     --type_moe_parent Qwen2MoeDecoderLayer \
-    #     --type_moe Qwen2MoeSparseMoeBlock \
-    #     --router_tensor_path gate \
-    #     --name_experts experts \
-    #     --world_size $world_size \
-    #     --enable_router_skew $enable_skew \
-    #     --router_skew $skew \
-    #     --router_num_experts_skewed $num_experts_skewed \
-    #     --pa "outputs/exp-qwen-systems-skew/$datetime/$skew-deepspeed"
+
+    deepspeed --num_gpus $world_size src/start_deepspeed.py \
+        --dataset "constant" \
+        --num_samples $num_samples \
+        --batch_size ${deepspeed_batches[$skew_index]} \
+        --seq_len $seq_len \
+        --model_name "Qwen/Qwen1.5-MoE-A2.7B-Chat" \
+        --num_experts 60 \
+        --d_model 2048 \
+        --type_moe_parent Qwen2MoeDecoderLayer \
+        --type_moe Qwen2MoeSparseMoeBlock \
+        --router_tensor_path gate \
+        --name_experts experts \
+        --world_size $world_size \
+        --enable_router_skew $enable_skew \
+        --router_skew $skew \
+        --router_num_experts_skewed $num_experts_skewed \
+        --pa "outputs/exp-qwen-systems-skew/$datetime/$skew-deepspeed"
 done
