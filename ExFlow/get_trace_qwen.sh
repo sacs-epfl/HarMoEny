30ee53d0a36134acc4d100c8cae5c350ed03bae1
Shamauk <zacharydoucet01@hotmail.com>
2026-02-17 00:37:14 -0500

    Updated ExFlow scripts to be more clear and validated all runs and created artifacts folder to store artifacts

---
+++ ExFlow/get_trace_qwen.sh
@@ -0,0 +1,18 @@
+python3 ../src/start_harmony.py \
+        --dataset bookcorpus \
+        --num_samples 510 \
+        --batch_size 85 \
+        --seq_len 60 \
+	--model_name "Qwen/Qwen1.5-MoE-A2.7B-Chat" \
+	--loader transformers \
+	--d_model 2048 \
+	--num_experts 60 \
+	--type_moe_parent Qwen2MoeDecoderLayer \
+	--type_moe Qwen2MoeSparseMoeBlock \
+	--router_tensor_path gate \
+	--name_experts experts \
+        --scheduling_policy "deepspeed" \
+        --expert_cache_size 10 \
+        --world_size 6 \
+        --warmup_rounds 0 \
+        --path "./raw_trace/qwen"
--- ExFlow/get_trace_switch.sh
+++ ExFlow/get_trace_switch.sh
@@ -1,14 +1,12 @@
-num_experts=$1
-echo "Running trace with $1 number of experts"
-
 python3 ../src/start_harmony.py \
         --dataset bookcorpus \
         --num_samples 680 \
         --batch_size 85 \
         --seq_len 60 \
-        --model_name "google/switch-base-$num_experts" \
+        --model_name "google/switch-base-128" \
+	--num_experts 128 \
         --scheduling_policy "deepspeed" \
         --expert_cache_size 16 \
         --world_size 8 \
         --warmup_rounds 0 \
-        --path "./raw_trace/$num_experts"
+        --path "./raw_trace/switch-128"
